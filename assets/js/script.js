5d43d54da32d330cbde9b2149dd731a688f04e20
LeShush <mycelium.architecture@gmail.com>
2023-04-25 15:31:37 -0600

    JS adn HTML updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 var apiKey = "c6ed5d250e6cffe2e326fd686c662f80";
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
